660c51738b752aa3b66aaab39b9ad40f81d21714
Alexey Karpov <karpovad@yandex.ru>
2015-11-09 00:41:06 +0100

    Отлавливаются события нажатия кнопки

--- src/plots/example1/index.js
+++ src/plots/example1/index.js
@@ -1,13 +1,35 @@
 import {Plotter} from 'sheckley';
 import getSomething from './get-something.js';
 import {ACTION, emitter} from 'store/eventer';
+import {BUTTON_DOWN, BUTTON_UP} from 'constants/buttonActions';
+
+let plotter;
+let func;
 
 export default function (id) {
-  let a = getSomething();
-  let s = new Plotter(id);
-  s.addFunc(x => getSomething(x));
+  plotter = new Plotter(id);
+  func = plotter.addFunc(x => getSomething(x));
 
+  events();
+};
+
+let counter = 0;
+let buttonsHandler = function (action) {
+  switch (action.id) {
+    case 'test-button':
+      plotter.remove(func);
+      counter++;
+      func = plotter.addFunc(x => getSomething(x) - counter);
+      break;
+  }
+};
+
+let events = function () {
   emitter.on(ACTION, action => {
-
+    switch (action.type) {
+      case BUTTON_UP:
+        buttonsHandler(action);
+        break;
+    }
   });
 };
